0e1eaa8dc5a46b475d493b20756047c6a22a2caa
kirtan kaushik <116414527+kt-pixels@users.noreply.github.com>
2024-02-08 12:41:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ const router = createBrowserRouter(
       {/* <Route element={<Private />}> */}
       <Route path="" element={<App />}>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<ClickImage />} />
+        <Route path="/images" element={<ClickImage />} />
         <Route path="/animals" element={<AllCtg />} />
         <Route path="/boys" element={<Boys />} />
         <Route path="/girls" element={<Girls />} />
